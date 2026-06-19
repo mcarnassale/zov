8e9a96e3f76d35a1d668a4ddf8c4a2029837ef2d
mcarnassale <marco@acrux1.com.br>
2026-06-19 15:21:09 -0300

    Atualização 2

--- webapp/test/integration/AllJourneys.js
+++ webapp/test/integration/AllJourneys.js
@@ -1,13 +1,25 @@
 sap.ui.define([
-	"sap/ui/test/Opa5",
-	"./arrangements/Startup",
-	"./NavigationJourney"
-], function (Opa5, Startup) {
+	"sap/ui/test/Opa5"
+], function (Opa5) {
 	"use strict";
 
-	Opa5.extendConfig({
-		arrangements: new Startup(),
-		viewNamespace: "zov.view.",
-		autoWait: true
+	return Opa5.extend("integration.arrangements.Startup", {
+
+		iStartMyApp: function (oOptionsParameter) {
+			var oOptions = oOptionsParameter || {};
+
+			// start the app with a minimal delay to make tests fast but still async to discover basic timing issues
+			oOptions.delay = oOptions.delay || 50;
+
+			// start the app UI component
+			this.iStartMyUIComponent({
+				componentConfig: {
+					name: "zov",
+					async: true
+				},
+				hash: oOptions.hash,
+				autoWait: oOptions.autoWait
+			});
+		}
 	});
 });
